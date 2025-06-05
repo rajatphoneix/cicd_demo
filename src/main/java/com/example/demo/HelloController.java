374884ef5cd2a3363db8c6f4d83d8ffe77555103
rajatphoneix <rajatphoenix@gmail.com>
2025-06-05 12:18:49 +0530

    Controller is modified

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, ModMed CI/CD!";
+        return "Hello there, ModMed CI/CD!";
     }
 }
