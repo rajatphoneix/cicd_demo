1019c046e73d6acc5b84db84e565c4c70134ab17
rajatphoneix <rajatphoenix@gmail.com>
2025-06-05 14:03:05 +0530

    Test class modified

--- src/test/java/com/modmed/patientservice/PatientServiceTest.java
+++
@@ -1,6 +0,0 @@
-@Test
-public void testGetPatientById() {
-    when(patientRepo.findById(1L)).thenReturn(Optional.of(new Patient(1L, "John")));
-    Patient patient = patientService.getPatientById(1L);
-    assertEquals("John", patient.getName());
-}
